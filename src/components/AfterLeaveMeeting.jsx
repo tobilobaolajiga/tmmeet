ac86c16fb2697457372363d499f09c351075e73f
tobilobaolajiga <tobilobaolajiga@gmail.com>
2024-04-26 11:35:54 +0100

    commit

--- src/components/AfterLeaveMeeting.jsx
+++ src/components/AfterLeaveMeeting.jsx
@@ -1,5 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-export default function AfterLeaveMeeting() {
+export default function AfterLeaveMeeting({
+  isVideoOn,
+  isAudioOn,
+  displayName,
+  meetingName,
+}) {
   const keysToRemove = [
     'meeting',
     'meetingCode',
@@ -23,23 +28,29 @@ export default function AfterLeaveMeeting() {
     });
     navigate('/');
   };
-  //   const rejoin = () => {
-  //     navigate(`/video/${meetingCode}`, {
-  //         state: {
-  //           isVideoOn,
-  //           isAudioOn,
-  //           displayName,
-  //           meetingName,
-  //         },
-  //       })
-  //   };
+  const meetingCode = localStorage.getItem('meetingCode');
+  const rejoin = () => {
+    navigate(`/video/${meetingCode}`, {
+      state: {
+        isVideoOn,
+        isAudioOn,
+        displayName,
+        meetingName,
+      },
+    });
+  };
 
   return (
     <div className="h-screen w-screen flex justify-center items-center">
       <div className=" items-center flex-col ">
         <p>You've left the meeting</p>
         <div className="flex gap-2 mt-4">
-          <button className="text-[#36aad9] bg-white px-4 py-2">Rejoin</button>
+          <button
+            className="text-[#36aad9] bg-white px-4 py-2"
+            onClick={rejoin}
+          >
+            Rejoin
+          </button>
           <button
             className="bg-[#36aad9] text-white px-4 py-2 rounded"
             onClick={goHome}
