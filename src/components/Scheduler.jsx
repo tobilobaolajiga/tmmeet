3fc261fb35fb795d311257191b0ead67b490ea09
tobilobaolajiga <tobilobaolajiga@gmail.com>
2024-04-26 11:05:12 +0100

    commit

--- src/components/Scheduler.jsx
+++ src/components/Scheduler.jsx
@@ -126,7 +126,7 @@ export default function Scheduler({
   const allGuests = JSON.stringify(guests);
 
   const scheduleMeeting = async () => {
-    console.log('clicked');
+    console.log(allGuests);
     setLoading(true);
     const startTime = `${selectedDate.getFullYear()}-${(
       selectedDate.getMonth() + 1
