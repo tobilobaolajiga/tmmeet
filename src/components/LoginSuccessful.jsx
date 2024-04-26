22b31ce26030c40b9863978e5e5c87aad7170523
tobilobaolajiga <tobilobaolajiga@gmail.com>
2024-04-26 11:38:22 +0100

    commit

--- src/components/LoginSuccessful.jsx
+++ src/components/LoginSuccessful.jsx
@@ -74,7 +74,7 @@ export default function LoginSuccessful({
             products={products}
           />
         </div>
-        <div className="flex bg-white">
+        <div className="flex bg-white items-center justify-between">
           <div className="px-[55px] py-[160px]">
             <div className="flex flex-col gap-6 font-DMSans basis-[1000px]">
               <div className=" text-[39px] font-bold">
